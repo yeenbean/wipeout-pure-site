a1944464a47e045b55cf893b920c5672b8dcdb68
yeenbean <yeenimal@pm.me>
2023-11-25 21:15:43 -0700

    Redid most of the CSS and HTML for better portability and pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,10 +1,10 @@
 // functions
 function hideAll() {
-    let entry = document.getElementById("entry");
+    let entry = document.getElementById("preload");
     let mainMenu = document.getElementById("main-menu");
 
-    entry.style.visibility = "hidden";
-    mainMenu.style.visibility = "hidden";
+    entry.style.display = "none";
+    mainMenu.style.display = "none";
 }
 
 function gotoMainMenuFirstTime() {
@@ -12,6 +12,6 @@ function gotoMainMenuFirstTime() {
     let audio = document.getElementById("curves");
 
     hideAll();
-    mainMenu.style.visibility = "visible";
+    mainMenu.style.display = "block";
     audio.play();
 }
