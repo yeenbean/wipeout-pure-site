11c1080e096d2578759d40ec9f65884130e18b46
zima <yeenimal@pm.me>
2023-12-02 11:36:25 -0700

    ✨️🔧 Update start-playground.sh script to fallback to node
    This commit updates the start-playground.sh script to use node when bun is missing from the system. If either of these commands is not found, an error message will be displayed.
    
    The effects of this commit are:
    - Improved usability by providing alternative options for starting an HTTP server
    - Better error handling when required dependencies are missing

--- start-playground.sh
+++ start-playground.sh
@@ -1,10 +1,18 @@
 if ! command -v bun &> /dev/null; then
-    echo "bun is missing from your system."
-    echo "If you don't want to install bun, simply start an http server using"
-    echo "this directory as the root."
-    echo "If you want to use bun, visit https://bun.sh for installation instructions."
-    exit 1
+    if ! command -v npm &> /dev/null; then
+        echo "bun and node are missing from your system."
+        echo "If you don't want to install them, simply start an http server"
+        echo "using this directory as the root."
+        echo
+        echo "If you want to use bun, visit https://bun.sh for"
+        echo "installation instructions."
+        exit 1
+    else
+        npm i -g http-server
+        npx http-server -c-1
+        exit 0
+    fi
+else
+    bun install -g http-server
+    bunx http-server -c-1
 fi
-
-bun install -g http-server
-bunx http-server -c-1
